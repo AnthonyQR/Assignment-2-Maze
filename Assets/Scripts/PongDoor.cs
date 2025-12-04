906068e942d02101524b229fbbbdcfadb2471bf6
Patrick Ng <patrickng995@gmail.com>
2025-12-03 20:20:08 -0800

    Disabled fog when going into pong

--- Assets/Scripts/PongDoor.cs
+++ Assets/Scripts/PongDoor.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class PongDoor : MonoBehaviour
 {
+    [SerializeField] private FullScreenPassRendererFeature _fogRendererFeature;
+
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player"))
         {
+            _fogRendererFeature.SetActive(false);
             SceneManager.LoadScene("GameScene");
         } 
     }
